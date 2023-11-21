bcdc94c469781be96826bc3d7751a8ef3da0a4d4
MartinTye <martinhtye@gmail.com>
2023-11-21 18:06:48 +0100

    heh

--- interactableArea.gd
+++ interactableArea.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var playerInside : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +8,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if playerInside and Input.is_action_just_pressed("Interact"):
+		_on_interact_pressed()
+
+
+func _on_interactable_area_body_entered(body):
+	if body.name == "Player":
+		playerInside = true
+		print("entered")
+
+
+func _on_interactable_area_body_exited(body):
+	if body.name == "Player":
+		playerInside = false
+	
+func _on_interact_pressed():
+	print("hi")
