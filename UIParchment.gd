54ff6fa3959389942c08d20f1a4ae473f2839379
Flem1000 <flemmingcai@yahoo.com>
2023-11-23 14:50:29 +0100

    PAGeS!!!

--- UIParchment.gd
+++ UIParchment.gd
@@ -2,33 +2,43 @@ extends interactableUI
 
 var area = get_owner()
 var page : int = 0
+var pages: Array
 @export var text : String
 @onready var label = $CanvasLayer/Page1
+@onready var pagePrev = $CanvasLayer/PagePrev
+@onready var pageNext = $CanvasLayer/PageNext
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(label.text)
-	var pages = label.text.split("|", false)
-	print(pages)
-	for page in pages:
-		label.append_text("[p]"+page+"[/p]")
-		for i in range(1):
-			label.newline()
-	label.set_scroll_active(false)
+	pages = text.split("|", false)
+	label.text = pages[page]
+#	print(pages)
+#	for page in pages:
+#		label.append_text("[p]"+page+"[/p]")
+#		for i in range(1000):
+#			label.newline()
+#	label.set_scroll_active(false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if page == 0:
+		pagePrev.hide()
+	elif page == pages.size()-1:
+		pageNext.hide()
+	else:
+		pagePrev.show()
+		pageNext.show()
+		
 
 func _on_page_prev_pressed():
 	page -= 1
-	label.scroll_to_paragraph(page)
+	label.text = pages[page]
 
 func _on_page_next_pressed():
 	page += 1
-	label.scroll_to_paragraph(page)
+	label.text = pages[page]
 
 
 
