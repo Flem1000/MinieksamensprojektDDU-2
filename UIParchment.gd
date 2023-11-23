ebb58ac9e41e3f7c384678e5a5c92479046c064b
Flem1000 <flemmingcai@yahoo.com>
2023-11-23 11:29:10 +0100

    Shenanigans

--- UIParchment.gd
+++ UIParchment.gd
@@ -1,19 +1,34 @@
 extends interactableUI
 
 var area = get_owner()
+var page : int = 0
 @export var text : String
-@onready var label = $CanvasLayer/Label
+@onready var label = $CanvasLayer/Page1
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	await get_tree().create_timer(0.1).timeout
-	label.text = text
-	
+	print(label.text)
+	var pages = label.text.split("|", false)
+	print(pages)
+	for page in pages:
+		label.append_text("[p]"+page+"[/p]")
+		for i in range(1):
+			label.newline()
+	label.set_scroll_active(false)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func _on_page_prev_pressed():
+	page -= 1
+	label.scroll_to_paragraph(page)
+
+func _on_page_next_pressed():
+	page += 1
+	label.scroll_to_paragraph(page)
 
 
-	
+
